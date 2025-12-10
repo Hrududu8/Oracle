1d685bb99e6cb2e58118f59b97cfe9f26d902418
rk <rkorde@gmail.com>
2025-12-09 21:47:53 -0500

    Initial Commit

---
+++ Oracle/OracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OracleApp.swift
+//  Oracle
+//
+//  Created by Rukesh Korde on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct OracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
